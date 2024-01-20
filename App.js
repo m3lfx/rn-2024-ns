9f7178bc099e58050c5928b543ca848e45cbb378
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-20 10:57:20 +0800

    products

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import ProductContainer from './Screens/Product/ProductContainer';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello mobile</Text>
-      <StatusBar style="auto" />
+      <ProductContainer />
     </View>
   );
 }
---
+++ Screens/Product/ProductContainer.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native'
+
+const ProductContainer = () => {
+
+    return (
+        <View>
+            <Text>Product container</Text>
+        </View>
+    )
+}
+
+export default ProductContainer;
