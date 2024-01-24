cc4b8a5d49459eb9d19cd6632e0da0e6ff3a3ad2
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-24 10:54:00 +0800

    bottom tab

---
+++ Navigators/Main.js
@@ -0,0 +1,83 @@
+import React, { useContext } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const Tab = createBottomTabNavigator();
+
+const Main = () => {
+    return (
+        <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={ {
+                tabBarHideOnKeyboard: true,
+                tabBarShowLabel: false,
+                tabBarActiveTintColor: '#e91e63'
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarIcon: ({color}) => {
+                        return <Icon
+                            name="home"
+                            style={{position: "relative"}}
+                            color={color}
+                            size={30}
+
+                        />
+                    }
+                }}
+            />
+
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarIcon: ({color}) => {
+                        return <Icon
+                            name="shopping-cart"
+                            style={{position: "relative"}}
+                            color={color}
+                            size={30}
+
+                        />
+                    }
+                }}
+            />
+
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarIcon: ({color}) => {
+                        return <Icon
+                            name="cog"
+                            style={{position: "relative"}}
+                            color={color}
+                            size={30}
+
+                        />
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="User"
+                component={Home}
+                options={{
+                    tabBarIcon: ({color}) => {
+                        return <Icon
+                            name="user"
+                            style={{position: "relative"}}
+                            color={color}
+                            size={30}
+
+                        />
+                    }
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
+export default Main
