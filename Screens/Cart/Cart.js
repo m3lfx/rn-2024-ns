1b4ab78c9ab1d82573c71902c398789ba36a3163
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-31 09:51:07 +0800

    redux cart

--- Screens/Cart/Cart.js
+++ Screens/Cart/Cart.js
@@ -1,9 +1,23 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {Text, View} from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigation } from '@react-navigation/native';
+import * as actions from '../../Redux/Actions/cartActions'
 
 const Cart =() => {
+    const navigation = useNavigation()
+    dispatch = useDispatch()
+    const cartItems = useSelector(state => state.cartItems)
+   
     return (
-        <Text>It works</Text>
+        <View style={{flex:1}}>
+        {cartItems.map(x => {
+             console.log(x)
+          return(
+            <Text>{x.name}</Text>
+          )
+        })}
+      </View>
     )
 }
 
