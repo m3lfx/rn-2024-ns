b142c025768e0bf79eec6c797cc6980e03fa1e65
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-31 08:50:55 +0800

    redux store

---
+++ Redux/store.js
@@ -0,0 +1,14 @@
+import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+import cartItems from './Reducers/cartItems';
+const reducers = combineReducers({
+    // cartItems: cartItems
+})
+
+const store = createStore(
+    reducers,
+    applyMiddleware(thunkMiddleware),
+)
+
+export default store;
