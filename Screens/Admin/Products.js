f73381fbec875375885e0eaaecf36773eacdcdbf
rommel dalisay <rommelmdalisay@gmail.com>
2024-02-07 09:58:34 +0800

    product list

--- Screens/Admin/Products.js
+++ Screens/Admin/Products.js
@@ -1,12 +1,147 @@
-import React from "react";
-import {View, Text} from 'react-native'
+import React, { useState, useCallback } from "react";
+import {
+    View,
+    Text,
+    FlatList,
+    ActivityIndicator,
+    StyleSheet,
+    Dimensions,
+    
+} from "react-native";
+import { Input, VStack, Heading, Box } from "native-base"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { useFocusEffect } from "@react-navigation/native"
+import { Searchbar } from 'react-native-paper';
+import ListItem from "./ListItem"
+
+import axios from "axios"
+import baseURL from "../../assets/common/baseurl"
+import AsyncStorage from '@react-native-async-storage/async-storage'
+var { height, width } = Dimensions.get("window")
 
 const Products = (props) => {
-    return (
-        <View>
-            <Text>Products</Text>
-        </View>
-    )
-} 
 
-export default Products
+    const [productList, setProductList] = useState([]);
+    const [productFilter, setProductFilter] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [token, setToken] = useState();
+    const ListHeader = () => {
+        return (
+            <View
+                elevation={1}
+                style={styles.listHeader}
+            >
+                <View style={styles.headerItem}></View>
+                <View style={styles.headerItem}>
+                    <Text style={{ fontWeight: '600' }}>Brand</Text>
+                </View>
+                <View style={styles.headerItem}>
+                    <Text style={{ fontWeight: '600' }}>Name</Text>
+                </View>
+                <View style={styles.headerItem}>
+                    <Text style={{ fontWeight: '600' }}>Category</Text>
+                </View>
+                <View style={styles.headerItem}>
+                    <Text style={{ fontWeight: '600' }}>Price</Text>
+                </View>
+            </View>
+        )
+    }
+    const searchProduct = (text) => {
+        if (text === "") {
+            setProductFilter(productList)
+        }
+        setProductFilter(
+            productList.filter((i) => 
+                i.name.toLowerCase().includes(text.toLowerCase())
+            )
+        )
+    }
+    useFocusEffect(
+        useCallback(
+            () => {
+                // Get Token
+                AsyncStorage.getItem("jwt")
+                    .then((res) => {
+                        setToken(res)
+                    })
+                    .catch((error) => console.log(error))
+                axios
+                    .get(`${baseURL}products`)
+                    .then((res) => {
+                        console.log(res.data)
+                        setProductList(res.data);
+                        setProductFilter(res.data);
+                        setLoading(false);
+                    })
+
+                return () => {
+                    setProductList();
+                    setProductFilter();
+                    setLoading(true);
+                }
+            },
+            [],
+        )
+    )
+    return (
+        <Box flex={1}>
+            <Searchbar width="80%"
+                placeholder="Search"
+                onChangeText={(text) => searchProduct(text)}
+            //   value={searchQuery}
+            />
+            {loading ? (
+                <View style={styles.spinner}>
+                    <ActivityIndicator size="large" color="red" />
+                </View>
+            ) : ( <FlatList
+                ListHeaderComponent={ListHeader}
+                data={productFilter}
+                renderItem={({ item, index }) => (
+                    <ListItem
+                        item={item}
+                        index={index}
+                    // deleteProduct={deleteProduct}
+
+                    />
+                )}
+                keyExtractor={(item) => item.id}
+            />)}
+
+                
+        </Box>
+    );
+}
+
+const styles = StyleSheet.create({
+    listHeader: {
+        flexDirection: 'row',
+        padding: 5,
+        backgroundColor: 'gainsboro'
+    },
+    headerItem: {
+        margin: 3,
+        width: width / 6
+    },
+    spinner: {
+        height: height / 2,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    container: {
+        marginBottom: 160,
+        backgroundColor: 'white'
+    },
+    buttonContainer: {
+        margin: 20,
+        alignSelf: 'center',
+        flexDirection: 'row'
+    },
+    buttonText: {
+        marginLeft: 4,
+        color: 'white'
+    }
+})
+
+export default Products;
