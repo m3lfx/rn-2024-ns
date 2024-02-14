10458c69d9f9056eb97f0329f2cb458a994c7c24
rommel dalisay <rommelmdalisay@gmail.com>
2024-02-14 10:55:21 +0800

    expo camera

--- Screens/User/Register.js
+++ Screens/User/Register.js
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet,  } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity, Dimensions  } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { useNavigation } from '@react-navigation/native';
-import { Button } from "native-base";
+import { Button, Center } from "native-base";
 import FormContainer from "../../Shared/Form/FormContainer";
 import Input from "../../Shared/Form/Input";
 import Error from "../../Shared/Error";
 import axios from "axios";
 import baseURL from "../../assets/common/baseurl";
 import Toast from "react-native-toast-message";
+import { Camera } from 'expo-camera';
+import Icon from "react-native-vector-icons/FontAwesome"
+import mime from "mime";
+var { height, width } = Dimensions.get("window")
 
 const Register = (props) => {
     const [email, setEmail] = useState("");
@@ -16,44 +20,112 @@ const Register = (props) => {
     const [phone, setPhone] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
+    const [launchCam, setLaunchCam] = useState(false)
+    const [type, setType] = useState(Camera.Constants.Type.back);
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
+    const [camera, setCamera] = useState(null);
+    const [image, setImage] = useState(null);
+    const [mainImage, setMainImage] = useState('');
     const navigation = useNavigation()
+
+    const addPhoto = async () => {
+        setLaunchCam(true)
+        if (camera) {
+            const data = await camera.takePictureAsync(null)
+            setImage(data.uri);
+            setMainImage(data.uri)
+            setLaunchCam(false)
+        }
+    }
 
     const register = () => {
         if (email === "" || name === "" || phone === "" || password === "") {
             setError("Please fill in the form correctly");
         }
-        let user = {
-            name: name,
-            email: email,
-            password: password,
-            phone: phone,
-            isAdmin: false,
-          };
-          axios
-            .post(`${baseURL}users/register`, user)
+        // let user = {
+        //     name: name,
+        //     email: email,
+        //     password: password,
+        //     phone: phone,
+        //     isAdmin: false,
+        //   };
+        //   axios
+        //     .post(`${baseURL}users/register`, user)
+        //     .then((res) => {
+        //       if (res.status == 200) {
+        //         Toast.show({
+        //           topOffset: 60,
+        //           type: "success",
+        //           text1: "Registration Succeeded",
+        //           text2: "Please Login into your account",
+        //         });
+        //         setTimeout(() => {
+        //           navigation.navigate("Login");
+        //         }, 500);
+        //       }
+        //     })
+        //     .catch((error) => {
+        //       Toast.show({
+        //         position: 'bottom',
+        //         bottomOffset: 20,
+        //         type: "error",
+        //         text1: "Something went wrong",
+        //         text2: "Please try again",
+        //       });
+        //     });
+        let formData = new FormData();
+        const newImageUri = "file:///" + image.split("file:/").join("");
+
+
+        formData.append("name", name);
+        formData.append("email", email);
+        formData.append("password", password);
+        formData.append("phone", phone);
+        formData.append("isAdmin", false);
+        formData.append("image", {
+            uri: newImageUri,
+            type: mime.getType(newImageUri),
+            name: newImageUri.split("/").pop()
+        });
+        const config = {
+            headers: {
+                "Content-Type": "multipart/form-data",
+
+            }
+        }
+        axios
+            .post(`${baseURL}users/register`, formData, config)
             .then((res) => {
-              if (res.status == 200) {
-                Toast.show({
-                  topOffset: 60,
-                  type: "success",
-                  text1: "Registration Succeeded",
-                  text2: "Please Login into your account",
-                });
-                setTimeout(() => {
-                  navigation.navigate("Login");
-                }, 500);
-              }
+                if (res.status === 200) {
+                    Toast.show({
+                        topOffset: 60,
+                        type: "success",
+                        text1: "Registration Succeeded",
+                        text2: "Please Login into your account",
+                    });
+                    setTimeout(() => {
+                        navigation.navigate("Login");
+                    }, 500);
+                }
             })
             .catch((error) => {
-              Toast.show({
-                position: 'bottom',
-                bottomOffset: 20,
-                type: "error",
-                text1: "Something went wrong",
-                text2: "Please try again",
-              });
-            });
+                Toast.show({
+                    position: 'bottom',
+                    bottomOffset: 20,
+                    type: "error",
+                    text1: "Something went wrong",
+                    text2: "Please try again",
+                });
+                console.log(error.message)
+            })
     }
+
+    useEffect(() => {
+        (async () => {
+            const cameraStatus = await Camera.requestCameraPermissionsAsync();
+            setHasCameraPermission(cameraStatus.status === 'granted');
+        })();
+    }, []);
 
     return (
         <KeyboardAwareScrollView
@@ -62,6 +134,41 @@ const Register = (props) => {
             enableOnAndroid={true}
         >
             <FormContainer title={"Register"}>
+            {launchCam ?
+                    <Center width={width} >
+                        <View style={styles.cameraContainer}>
+                            <Camera
+                                ref={ref => setCamera(ref)}
+                                style={styles.fixedRatio}
+                                type={type}
+                                ratio={'1:1'} />
+
+                        </View>
+                        <Button
+                            variant={"ghost"}
+                            onPress={() => addPhoto()}><Text> add photo</Text>
+                        </Button>
+                        <Button
+                            variant={"ghost"}
+                            onPress={() => {
+                                setType(
+                                    type === Camera.Constants.Type.back
+                                        ? Camera.Constants.Type.front
+                                        : Camera.Constants.Type.back
+                                );
+                            }}><Text>flip camera</Text>
+                        </Button>
+
+                    </Center> : null}
+
+                <View style={styles.imageContainer}>
+                    <Image style={styles.image} source={{ uri: mainImage }} />
+                    <TouchableOpacity
+                        onPress={addPhoto}
+                        style={styles.imagePicker}>
+                        <Icon style={{ color: "white" }} name="camera" />
+                    </TouchableOpacity>
+                </View>
                 <Input
                     placeholder={"Email"}
                     name={"email"}
@@ -114,6 +221,45 @@ const styles = StyleSheet.create({
         margin: 10,
         alignItems: "center",
     },
+    buttonContainer: {
+        width: "80%",
+        marginBottom: 80,
+        marginTop: 20,
+        alignItems: "center"
+    },
+    imageContainer: {
+        width: 200,
+        height: 200,
+        borderStyle: "solid",
+        borderWidth: 8,
+        padding: 0,
+        justifyContent: "center",
+        borderRadius: 100,
+        borderColor: "#E0E0E0",
+        elevation: 10
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+        borderRadius: 100
+    },
+    imagePicker: {
+        position: "absolute",
+        right: 5,
+        bottom: 5,
+        backgroundColor: "grey",
+        padding: 8,
+        borderRadius: 100,
+        elevation: 20
+    },
+    cameraContainer: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    fixedRatio: {
+        flex: 1,
+        aspectRatio: 1
+    }
 });
 
 export default Register;
